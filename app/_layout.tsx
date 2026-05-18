3cdcee029a51175d570cf3a8010eb4216e055605
xAstraly <couillardmathieu084@gmail.com>
2026-05-18 10:41:59 +0200

    update root layout to wrap app with AuthContext provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,23 @@
+import MyFooter from '@/components/footer';
 import MyHeader from '@/components/header';
-import { Slot } from 'expo-router';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { AuthProvider } from '../context/AuthContext';
+
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <View style={{ flex: 1 }}>
-        <MyHeader />
-        <Slot />
-      </View>
-    </SafeAreaProvider>
+    <AuthProvider>
+      <SafeAreaProvider>
+        <StatusBar style="dark" backgroundColor="#ffffff" />
+        <View style={{ flex: 1 }}>
+          <MyHeader />
+          <Stack screenOptions={{ headerShown: false }}/>
+          <MyFooter />
+        </View>
+      </SafeAreaProvider>
+    </AuthProvider>
   );
 }
